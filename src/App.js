9fdfad850cf2064b7e1bd0bf942ee0dae44fd245
Valentino Hariyanto <117331875+valentinohrynt@users.noreply.github.com>
2026-08-14 21:38:29 +0700

    chore: simplify app entry point

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import Portfolio from './components/Portfolio';
 import './index.css';
-import 'devicon/devicon.min.css';
-
 
 function App() {
-  return (
-    <div>
-      <Portfolio />
-    </div>
-  );
+  return <Portfolio />;
 }
 
 export default App;
